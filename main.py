7a00e0d5aebe7bd91c751828cb9c15c67baef320
Jake Springer <jakebob.dev@gmail.com>
2023-02-28 12:02:14 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -70,9 +70,12 @@ def create_profile():
         profile = {
             "name":profile_name,
             "balance":start_bal,
-            "bet":0,
+            "bet":10,
             "wins":0,
             "losses":0,
+            "draws":0,
+            "total_games":0,
+            "gain":0,
             "busts":0
         }
         data["profiles"].append(profile)
@@ -109,8 +112,15 @@ def select_profile():
     # User input as the index value from profile_list
     for name in profile_list:
         print(f"[{str(profile_list.index(name) + 1)}]. {name['name']}")
-    select = input("\n[?] ")
-    return profile_list[int(select) - 1]
+    print()
+    while True:
+        select = input("[?] ")
+        try:
+            return profile_list[int(select) - 1]
+        except:
+            print("[X] Not valid.")
+            pass
+
 
 
 def refresh_profile(player):
@@ -139,6 +149,23 @@ def change_bet(player):
     save_data(data)
 
 
+def view_stats(player):
+    clear()
+    print("\t///  Statistics  ///\n")
+    print("[~] Name: " + player['name'])
+    print("[~] Balance: $" + str(player['balance']))
+    print("[~] Total gain: $" + str(player['gain']))
+    print("[~] Total busts: " + str(player['busts']))
+    print()
+    print("[~] Total games: " + str(player["total_games"]))
+    print("[~] Total wins: " + str(player['wins']))
+    print("[~] Total losses: " + str(player['losses']))
+    print("[~] Total draws: " + str(player["draws"]))
+    print()
+    print("[~] Win/loss ratio: " + str(player['wins'] / player['losses']))
+    input("\n[~] Press enter to continue ")
+
+
 def profile_management_menu():
     while True:
         clear()
@@ -148,13 +175,45 @@ def profile_management_menu():
         print("[4]. Return to menu")
         select = input("\n[?] ")
         if select == '1':
-            input("wip") 
+            view_profiles()
         elif select == '2':
             create_profile()
         elif select == '3':
-            input("wip")
+            delete_profile()
         elif select == '4':
             return
+
+
+def view_profiles():
+    data = load_data()
+    profiles = data["profiles"]
+    clear()
+    for p in profiles:
+        print("~ Name:  " + p["name"])
+        print("~ Balance: $" + str(p["balance"]))
+        print("~ Games: " + str(p["wins"] + p["losses"]))
+        print()
+    input("[~] Press enter to continue. ")
+
+
+def delete_profile():
+    clear()
+    print("[X] ENTERED DELETE PROFILE MENU")
+    print("[!] Changes cannot be reversed!")
+    print("[~] Confirm?")
+    conf = input("\n(Y/N) ")
+    while conf.lower() != 'y' and conf.lower() != 'n':
+        conf = input("\n(Y/N) ")
+    if conf.lower() == 'n':
+        return
+    elif conf.lower() == 'y':
+        prof = select_profile()
+        data = load_data()
+        i = data["profiles"].index(prof)
+        data["profiles"].pop(i)
+        save_data(data)
+        print("\n[!] Profile deleted! ")
+        input("[~] Press enter to continue. ")
 
 #--------------------------------------------------------
 #? Game loop
@@ -189,9 +248,30 @@ def player_pick():
     return int(select)
 
 
-def play_menu(player): #!WIP not functional yet
+def bust(player):
+    clear()
+    data = load_data()
+    for profile in data["profiles"]:
+        if profile["name"] == player["name"]:
+            profile["busts"] += 1
+            profile["balance"] += 100
+    save_data(data)
+    print("[!] YOU BUSTED!")
+    print("[!] Your account has reached $0 :(")
+    print("\n[~] However, since this is just a game, $100 will be")
+    print("    added to your account. But, your \"busts\" statistic")
+    print("    will go up by one, as a reminder to your poor money")
+    print("    management skills. ")
+    input("\n[~] Press enter to continue. ")
+    
+
+
+def play_menu(player): 
     while True:
         player = refresh_profile(player)
+        if player["balance"] == 0:
+            bust(player)
+            return
         clear()
         print("[!] Player: " + player["name"])
         print("[!] Balance: $" + str(player["balance"]))
@@ -202,11 +282,17 @@ def play_menu(player): #!WIP not functional yet
         print("[4]. Return to menu")
         select = input("\n[?] ")
         if select == '1':
-            play(player)
+            if player["bet"] > player["balance"]:
+                clear()
+                print("[X] You don't have enough to cover this bet!")
+                print("[X] Change your bet to something you can afford.")
+                input("\n[!] Press enter to continue.")
+            else:
+                play(player)
         elif select == '2':
             change_bet(player)
         elif select == "3":
-            pass
+            view_stats(player)
         elif select == "4":
             return
 
@@ -218,11 +304,12 @@ def lose(player):
         if prof["name"] == player["name"]:
             target_player = prof
     # Alter json profile values
+    target_player["total_games"] += 1
     target_player["losses"] += 1
     target_player["balance"] -= player["bet"]
-    if target_player["balance"] == 0:
-        target_player["busts"] += 1
-
+    target_player["gain"] -= player['bet']
+    # if target_player["balance"] == 0:
+    #     target_player["busts"] += 1
     save_data(data)
     
 
@@ -233,11 +320,23 @@ def win(player):
         if prof["name"] == player["name"]:
             target_player = prof
     # Alter json profile values
+    target_player["total_games"] += 1
     target_player["wins"] += 1
     target_player["balance"] += player["bet"]
-
+    target_player['gain'] += player['bet']
     save_data(data)
     
+
+def draw(player):
+    # Find profile in JSON by name
+    data = load_data()
+    for prof in data["profiles"]:
+        if prof["name"] == player["name"]:
+            target_player = prof
+    target_player["total_games"] += 1
+    target_player["draws"] += 1
+    save_data(data)
+
 
 def play(player):
     clear()
@@ -245,26 +344,96 @@ def play(player):
     cpu_choice = cpu_pick()
     print("\n[!] CPU is picking....")
     #sleep(2)
+    # Returns string, either "player","cpu", or "draw"
     winner = determine_winner(player_choice,cpu_choice)
     input("[!] Press enter to see the results.")
     clear()
     print(f"[!] {player['name']} picked " + move_list[player_choice]["move_name"])
     print(f"[!] CPU picked " + move_list[cpu_choice]["move_name"])
+    # Player wins
     if winner == 'player':
         print(f"\n[!] {player['name']} wins!")
         print(f"[!] ${str(player['bet'])} has been added to your balance")
         win(player)
         input("\n[!] Press enter to continue")
+    # CPU wins
     elif winner == 'cpu':
         print("\n[!] CPU wins!")
         print(f"[!] ${str(player['bet'])} has been deducted from your balance")
         lose(player)
         input("\n[!] Press enter to continue")
+    # Draw, same selection
     elif winner == 'draw':
         print("\n[!] Draw!")
         print("[!] Nothing lost, nothing gained.")
+        draw(player)
         input("[!] Press enter to continue")
 
+#--------------------------------------------------------
+#? Settings
+
+def change_start_bal():
+    data = load_data()
+    current_bal = data["settings"]["start_bal"]
+    clear()
+    print("[!] Changing starting balance.")
+    print("[!] Current value: $" + str(current_bal))
+    print("[!] Enter the new balance:")
+    new_bal = input("\n[?] $")
+    data["settings"]["start_bal"] = int(new_bal)
+    save_data(data)
+    print("\n[!] Settings updated")
+    input("[~] Press enter to continue ")
+    
+
+def toggle_throw_pause():
+    data = load_data()
+    tp_value = data["settings"]["throw_pause"]
+    data["settings"]["throw_pause"] = not tp_value
+    save_data(data)
+
+
+def delete_profile_data():
+    clear()
+    print("[X] YOU ARE ABOUT TO DELETE ALL PROFILES")
+    print("[!] Are you sure you want to continue?")
+    conf = input("\n(Y/N) ")
+    while conf.lower() != 'y' and conf.lower() != 'n':
+        conf = input("(Y/N) ")
+    if conf.lower() == 'y':
+        data = load_data()
+        data["profiles"] = []
+        save_data(data)
+        print("\n[!] Save data has been deleted.")
+        input("[~] Press enter to continue. ")
+    elif conf.lower() == 'n':
+        print("\n[!] Cancelled")
+        input("[~] Press enter to continue. ")        
+
+
+def settings_menu():
+    while True:
+        data = load_data()
+        start_bal = data["settings"]["start_bal"]
+        if data["settings"]["throw_pause"]:
+            tp_state = 'ON'
+        else:
+            tp_state = 'OFF'
+        clear()
+        print("\t///  Settings  ///")
+        print(f"[1]. Change starting balance (${str(start_bal)})")
+        print(f"[2]. Toggle Throw Pause ({tp_state})")
+        print("[3]. Delete profile data")
+        print("[4]. Return to menu")
+        select = input("\n[?] ")
+        if select == '1':
+            change_start_bal()
+        elif select == '2':
+            toggle_throw_pause()
+        elif select == '3':
+            delete_profile_data()
+        elif select == '4':
+            return
 
 #--------------------------------------------------------
 
@@ -286,7 +455,7 @@ def main():
         elif select == '2':
             profile_management_menu() 
         elif select == '3':
-            pass
+            settings_menu()
 
 try:
     main()
